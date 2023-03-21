dee66d4c4f6f1f8b8785c46c1415015463dae685
Spencer Williams <spencer.williams@opensesame.com>
2023-03-21 15:05:21 -0700

    a new test, for outdated-module behavior

---
+++ services/tf-of-outdated-module/main.tf
@@ -0,0 +1,3 @@
+module "test" {
+  source = "git@github.com:spilliams/renovate-test-terraform-module.git?ref=1.0.0"
+}
---
+++ services/tg-of-outdated-module/terragrunt.hcl
@@ -0,0 +1,3 @@
+terraform {
+  source = "git@github.com:spilliams/renovate-test-terraform-module.git//?ref=1.0.0"
+}
